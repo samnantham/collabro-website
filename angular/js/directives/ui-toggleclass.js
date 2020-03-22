3a47afc2937b10bd9ba5103ae696643072089fa2
samnantham <34029616+samnantham@users.noreply.github.com>
2020-03-22 21:18:45 +0530

    new changes committed

--- angular/js/directives/ui-toggleclass.js
+++ angular/js/directives/ui-toggleclass.js
@@ -28,6 +28,21 @@ angular.module('app')
         }
     }
 })
+.directive("moveNextOnMaxlength", function() {
+    return {
+        restrict: "A",
+        link: function($scope, element) {
+            element.on("input", function(e) {
+                if(element.val().length == element.attr("maxlength")) {
+                    var $nextElement = element.next();
+                    if($nextElement.length) {
+                        $nextElement[0].focus();
+                    }
+                }
+            });
+        }
+    }
+})
 
 .directive('focusMe', function($timeout, $parse) {
     return {
